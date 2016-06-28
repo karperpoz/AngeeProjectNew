7448c056e6ddd312bd5a96518651407f2d16cb9e
karperpoz <karolina94@interia.eu>
2016-06-28 09:47:21 +0200

    commit1

--- app/src/main/java/com/example/karolina/angeeproject/MainActivity.java
+++ app/src/main/java/com/example/karolina/angeeproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.karolina.angeeproject;
 
+import android.os.AsyncTask;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.net.Uri;
@@ -7,23 +8,29 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
     NotificationCompat.Builder headNotVisibleNotification;
-    private static final int headNotVisibleId=1;
+    public static final int headNotVisibleId=1;
     NotificationCompat.Builder cryingNotification;
     private static final int cryingId=2;
 
-   /* public CommunicateWithPython interCommunication;*/
+   //public CommunicateWithPython interCommunication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*interCommunication=new CommunicateWithPython();*/
+        //interCommunication=new CommunicateWithPython();
+        //interCommunication.execute();
+
     }
 
     public void onSettingsClick(View view)
@@ -40,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     {
         /*headNotVisibleNotification.setVibrate();*/
         headNotVisibleNotification=new NotificationCompat.Builder(this);
-        headNotVisibleNotification.setAutoCancel(true);
+        //headNotVisibleNotification.setAutoCancel(true);
        /* Uri sound=Uri.parse("android.resource://")*/
         /*headNotVisibleNotification.setSound();*/
         headNotVisibleNotification.setSmallIcon(R.drawable.photo);
@@ -54,10 +61,23 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager headNotVisibleNM=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         headNotVisibleNM.notify(headNotVisibleId,headNotVisibleNotification.build());
     }
-    public void startCryingNotification(View view)
+    public void startHeadNotVisibleNotification()
     {
-        /*headNotVisibleNotification.setSound();*/
-        /*headNotVisibleNotification.setVibrate();*/
+        headNotVisibleNotification=new NotificationCompat.Builder(this);
+        headNotVisibleNotification.setAutoCancel(true);
+        headNotVisibleNotification.setSmallIcon(R.drawable.photo);
+        headNotVisibleNotification.setContentTitle("DANGER!");
+        headNotVisibleNotification.setContentText("HEAD NOT VISIBLE");
+
+        Intent reportAlarm=new Intent(this,ReportAlarm.class);
+        PendingIntent reportAlarmPendingIntent=PendingIntent.getActivity(this,0,reportAlarm,PendingIntent.FLAG_UPDATE_CURRENT);
+        headNotVisibleNotification.setContentIntent(reportAlarmPendingIntent);
+
+        NotificationManager headNotVisibleNM=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        headNotVisibleNM.notify(headNotVisibleId,headNotVisibleNotification.build());
+    }
+    public void startCryingNotification()
+    {
         cryingNotification=new NotificationCompat.Builder(this);
         cryingNotification.setAutoCancel(true);
         cryingNotification.setSmallIcon(R.drawable.photo);
@@ -71,4 +91,55 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager cryingNM=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         cryingNM.notify(cryingId,cryingNotification.build());
     }
+
+    public class CommunicateWithPython extends AsyncTask <Void, Void, Void>{
+
+        public  String serverIP = "192.168.1.3";//rasberry Pi IP?
+        public  int serverPort = 8074;//???
+        Socket s;
+        public DataInputStream dis=null;
+        public int message;
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+              s = new Socket(serverIP, serverPort);
+            } catch (Exception e) {
+                Log.i("AsyncTank", "doInBackgoung: Cannot create Socket");
+            }
+            if (s.isConnected()) {
+                try {
+                    dis = (DataInputStream) s.getInputStream();
+                    if(dis!=null)
+                    {
+                       message = dis.readInt();
+                       if(message==1)startHeadNotVisibleNotification();
+                       if(message==2)startCryingNotification();
+                       dis=null;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            try {
+                if (s.isConnected()){
+
+                    dis.close();
+                    s.close();
+                }
+            } catch (Exception e) {
+                Log.i("onPostExecute", "socket not closed");
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/karolina/angeeproject/ReportAlarm.java
+++ app/src/main/java/com/example/karolina/angeeproject/ReportAlarm.java
@@ -1,19 +1,31 @@
 package com.example.karolina.angeeproject;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
 public class ReportAlarm extends AppCompatActivity {
 
+
+    //public SendToPython sendToPythonObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_alarm);
-    }
+       // sendToPythonObject=new SendToPython();
+       // sendToPythonObject.execute();
+        }
     public void checkBoxClicked(View view)
     {
         boolean isChecked=((CheckBox)view).isChecked();
@@ -33,9 +45,11 @@ public class ReportAlarm extends AppCompatActivity {
     public void buttonClicked(View view)
     {
         TextView falseAlarmButtonText=(TextView)findViewById(R.id.falseAlarmButton);
+
         if(falseAlarmButtonText.getText()=="CONFIRM")
         {
-            /*send signal to Python*/
+
+            //sendToPythonObject.writeToStream(3);
         }
 
         Intent goToMain=new Intent (this, MainActivity.class);
@@ -43,5 +57,47 @@ public class ReportAlarm extends AppCompatActivity {
 
 
     }
+    public class SendToPython extends AsyncTask<Void, Void, Void> {
+
+        public  String serverIP = "";//rasberry Pi IP?
+        public  int serverPort = 8074;//???
+        Socket s;
+        public DataOutputStream dos=null;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            try {
+                s = new Socket(serverIP, serverPort);
+            } catch (Exception e) {
+                Log.i("AsyncTank", "doInBackgoung: Cannot create Socket");
+            }
+            if (s.isConnected()) {
+                try {
+                    dos = (DataOutputStream) s.getOutputStream();
+                    }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+        }
+        public void writeToStream(int sendMessage) {
+            try {
+                if (s.isConnected()){
+
+                    dos.writeInt(sendMessage);
+                    dos.close();
+                    s.close();
+                                    }
+            } catch (Exception e) {
+                Log.i("AsynkTask", "writeToStream : Writing failed");
+            }
+        }
+
+
+    }
+
 
 }
